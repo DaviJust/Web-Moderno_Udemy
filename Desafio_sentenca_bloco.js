e7b99206e67090b7649100716da49f89343d4a0e
Davi <112717111+DaviJust@users.noreply.github.com>
2022-09-07 13:11:29 -0300

    Add files via upload

---
+++ Desafio_sentenca_bloco.js
@@ -0,0 +1,10 @@
+#O
+
+{console.log
+    console.log("Esses sao os numeros impares");
+        console.log(1,3,5,7,9,11);}
+    
+
+    {console.log
+            console.log("Esses sao os numeros pares");
+        console.log("2 4 6 8 10")};
