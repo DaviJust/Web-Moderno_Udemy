6ad68df91f3c382ef33d0c4bb8fb42474ddf71c9
DaviJust <davijustinocouto@gmail.com>
2022-09-07 14:22:26 -0300

    Novos exercicios feitos

---
+++ Dados_4.js
@@ -0,0 +1,10 @@
+//const let variable as constant, but constante work in the same way as var or let, whorever constante don't let change the sentence
+let a = 3;
+
+// ...
+
+a = a + 10;
+
+// ...
+
+console.log(a);
