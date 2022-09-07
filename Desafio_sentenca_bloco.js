b3ee8337211dfb06b46e6f1f09671794d89806b9
DaviJust <davijustinocouto@gmail.com>
2022-09-07 13:43:31 -0300

    Novos execicios feitos

---
+++ Desafio_sentenca_bloco.js
@@ -0,0 +1,12 @@
+//Codigo simples de console.log do qual faz a maquina escrever os numeros
+//Primeiro trabalho do curso.
+//Usei apenas o codigo Console.log
+
+{console.log
+        console.log("Esses sao os numeros impares");
+            console.log(1,3,5,7,9,11)};
+        
+
+    {console.log
+            console.log("Esses sao os numeros pares");
+        console.log("2 4 6 8 10")};
