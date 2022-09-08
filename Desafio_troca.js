37fdf80aab1935424b05522880a71b104842318e
Davi <112717111+DaviJust@users.noreply.github.com>
2022-09-08 19:37:31 -0300

    Add files via upload

---
+++ Desafio_troca.js
@@ -0,0 +1,15 @@
+let a = 7;
+let b = 94;
+
+//After exchanged... a = 94 e b = 7
+//Exchange the variables
+
+// temp = a;
+// //temp temporary value for a variable
+// a = b;
+// b = temp;
+
+[a, b] = [b , a]
+
+console.log("Letter A as " + a);
+console.log("Letter B as " + b)
