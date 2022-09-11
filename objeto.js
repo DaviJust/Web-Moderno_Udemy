6d5373111a0137bfa70716becc9edb0f3845e616
DaviJust <davijustinocouto@gmail.com>
2022-09-11 09:21:49 -0300

    Aula de objetos

---
+++ objeto.js
@@ -0,0 +1,13 @@
+const prod1 = {}  //Chaves seria uma forma de escrevere o objeto
+prod1.nome = `Celular ultra mega legal` // par(Nome.valor)
+prod1.preco = 4998.90 // par(Nome.valor)
+prod1[`desconto legal`] = 0.4 // Don't use atributtes with space, try to avoid that
+
+console.log(prod1)
+
+const prod2 = {
+    nome: `Camisa polo`,
+    preco: 80
+}
+
+console.log(prod2)
