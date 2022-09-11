ac6ab1968f6e9bf56b421dd0e3a51c3b7d20dd58
DaviJust <davijustinocouto@gmail.com>
2022-09-11 09:23:59 -0300

    qualquer coisa

--- objeto.js
+++ objeto.js
@@ -1,6 +1,6 @@
 const prod1 = {}  //Chaves seria uma forma de escrevere o objeto
 prod1.nome = `Celular ultra mega legal` // par(Nome.valor)
-prod1.preco = 4998.90 // par(Nome.valor)
+prod1.preco = 4998.90 // par (Nome.valor)
 prod1[`desconto legal`] = 0.4 // Don't use atributtes with space, try to avoid that
 
 console.log(prod1)
