7bd8b068bfd6fa6e8f90753599583a0ac51523b3
DaviJust <davijustinocouto@gmail.com>
2022-09-11 16:29:47 -0300

    Novos exercicios feitos

---
+++ aritmeticos.js
@@ -0,0 +1,9 @@
+const [a, b, c, d] = [55, 4, 11, 16]
+
+const soma = a + b + c + d
+const subtracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2
+
+console.log(soma, subtracao, multiplicacao)
