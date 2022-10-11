333358b406b89bdcba0efe2ae63f1eacb02fdf1d
Davi <davijustinocouto@gmail.com>
2022-10-10 23:28:19 -0300

    Update criandoObjetos.js

--- Secao 7/criandoObjetos.js
+++ Secao 7/criandoObjetos.js
@@ -19,3 +19,20 @@ const p1 = new Produto(`Caneta`, 7.99, 0.15)
 const p2 = new Produto(`Notebook`, 2998.99, 0.25)
 console.log(p1.getPrecoComDesconto() , p2.getPrecoComDesconto())
 
+// Funcao Factory
+function criarFuncionario(nome, salarioBase, faltas) {
+    return {
+        nome,
+        salarioBase,
+        faltas,
+        getSalario() {
+            return(salarioBase / 30) * (30 - faltas)
+        }
+    }
+}
+const f1 = criarFuncionario(`joao`,8000,10)
+const f2 = criarFuncionario(`maria`,18000,24)
+console.log(f1.getSalario(), f2.getSalario())
+
+// object.create
+const filha = Object.create(null)
