95ad16257ad506dcd53c4fe54e314b4cd113477e
Davi <davijustinocouto@gmail.com>
2022-12-07 11:40:39 -0300

    Exericio Class

---
+++ Exercicios/dogDojo.js
@@ -0,0 +1,7 @@
+class dogs {
+    constructor(nomes, peso, raca){
+        nomes = ["Max", "Mel", "Sol"];
+        peso = [35.2, 18.6, 2.5];
+        raca = ["Labrador", "Vira latas", "Chiuaua"];
+    }
+}
