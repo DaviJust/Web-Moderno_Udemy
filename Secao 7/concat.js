d5d63fd1a1d5ec346618d6e0d9ed504b422affac
Davi <davijustinocouto@gmail.com>
2022-12-09 16:21:46 -0300

    Exercicios

---
+++ Secao 7/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Maria', 'Ariel']
+const filhos = ['Lucas', 'Pedro']
+const todos = filhas.concat(filhos)
+console.log(todos,filhas,filhos)
+
+console.log(['a','b'].concat([1,2], [3,4],5,[[6,7]]))
