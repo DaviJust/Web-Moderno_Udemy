8c7a7784ed638687494afaf41d86d9616c57c809
Davi <davijustinocouto@gmail.com>
2022-12-17 16:53:45 -0300

    New exercises

--- Exercicios/passandoParametro.js
+++ Exercicios/passandoParametro.js
@@ -1,3 +1,7 @@
 const saudacoes = require('./parametro')('Ana', 'Joao', 'Pedro')
 
 console.log(saudacoes)
+
+let saudades = module.exports = function(... nomes){
+    return nomes.map(nome => `boa semana ${nome}!`)
+}
