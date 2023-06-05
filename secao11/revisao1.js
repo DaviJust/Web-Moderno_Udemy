8b8ba25a8827e343b35fee63fe25d4af221c5e21
Davi <davijustinocouto@gmail.com>
2023-06-05 08:36:06 -0300

    fix: Organizing all folder´s names

---
+++ secao11/revisao1.js
@@ -0,0 +1,18 @@
+//let e const
+{
+
+    var a = 2
+    var b = 3
+    console.log(b)
+}
+
+//Template String
+const produto = 'iPad'
+console.log(`${produto} é caro!`)
+
+//Destructuring
+const [l, e, ...tras] = "Cod3r"
+console.log(l, e, tras)
+
+const [x, y] = [1, 2, 3]
+console.log(x, y)
