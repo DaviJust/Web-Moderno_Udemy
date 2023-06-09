e196c41994ad6b4ab4f2def3f3829d8dfa8ebe30
Davi <davijustinocouto@gmail.com>
2023-06-09 17:17:39 -0300

    Gulp runtime web project

--- gulp/css/gulpfile.js
+++ gulp/css/gulpfile.js
@@ -1,6 +1,6 @@
 const { parallel } = require('gulp')
 const gulp = require('gulp')
-const sass = require('gulp-sass')
+const sass = require('gulp-sass')(require('node-sass')); 
 const uglifycss = require('gulp-uglifycss')
 const concat = require('gulp-concat')
 
